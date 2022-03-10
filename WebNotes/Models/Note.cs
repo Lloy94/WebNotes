009bff03f199fccfc3b672b44548ba2c49397f4a
Илья Тимофеев <Mcdos14@ya.ru>
2022-03-10 23:42:47 +0500

    подготовка основных компонентов/создание модели заметки

---
+++ WebNotes/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebNotes.Models
+{
+    public class Note
+    {
+        public IdentityUser User;
+
+        [Required]
+        public string NoteInfo { get; set; }
+
+        public DateTimeOffset Date { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
