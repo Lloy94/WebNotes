42c2b4ded9e8a859998aecfbee4b6b43dd4d9362
Илья Тимофеев <Mcdos14@ya.ru>
2022-03-12 04:14:44 +0500

    Меняющаяся главная страница + правка кода

---
+++ WebNotes/Components/HomeInfoViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebNotes.Services.InSql;
+using WebNotes.ViewModels;
+
+namespace WebNotes.Components
+{
+    public class HomeInfoViewComponent : ViewComponent
+    {
+        private static InSqlNoteData _NoteService;
+        public HomeInfoViewComponent([FromServices] InSqlNoteData NoteService) { _NoteService = NoteService; }
+        public IViewComponentResult Invoke()
+        {
+            var orders = _NoteService.GetUserNotes(User.Identity!.Name).Result.ToList();
+            int count = orders.Count();
+            if (User.Identity?.IsAuthenticated == true)
+                return View("HomeInfo",count);
+            else return View();
+            
+        }
+    }
+}
